0ea21f76185fa3524fed76634f318a01fd2388a4
samupadilla <srpadilla@misena.edu.co>
2022-04-26 15:49:28 -0500

    primer commit

---
+++ intro1.js
@@ -0,0 +1,6 @@
+var arreglo=[];
+var objeto={};
+console.log((arreglo));
+console.log((objeto));
+console.log(typeof(arreglo));
+console.log(typeof(objeto));
---
+++ prueba.js
@@ -0,0 +1,14 @@
+console.log('COMENZAMOS CON JS');
+var x=1200;//=prompt('Ingrese numero');
+console.log('Usted'+x+'ingreso...'+x);
+console.log(`otra ${x} forma de ${x} concatenar`);
+
+babel javascript
+auto close tag
+color highlight
+ESLint
+indent rainbow
+live server
+Peacock
+Prettier
+Trailing Spaces
